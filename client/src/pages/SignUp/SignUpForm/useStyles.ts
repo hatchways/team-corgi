38ffe9ab34857d651369b5f371a57bc8bfbb8fba
C. S. Ale <c.s.alexiuk@gmail.com>
2021-06-15 15:44:27 -0400

    made fixes based off of PR feedback

--- client/src/pages/SignUp/SignUpForm/useStyles.ts
+++ client/src/pages/SignUp/SignUpForm/useStyles.ts
@@ -27,7 +27,7 @@ const useStyles = makeStyles((theme) => ({
     borderRadius: theme.shape.borderRadius,
     marginTop: 49,
     fontSize: 16,
-    backgroundColor: '#primary',
+    backgroundColor: theme.palette.primary.main,
     fontWeight: 'bold',
   },
 }));
