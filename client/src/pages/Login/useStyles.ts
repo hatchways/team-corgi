47f2a0fcbee5bdc26bef4f00a5260f49fadc2d42
C. S. Ale <c.s.alexiuk@gmail.com>
2021-06-16 14:02:14 -0400

    restarting due to pc issues, commit to backup

--- client/src/pages/Login/useStyles.ts
+++ client/src/pages/Login/useStyles.ts
@@ -1,5 +1,5 @@
 import { makeStyles } from '@material-ui/core/styles';
-import { theme } from '../../themes/theme';
+
 
 const useStyles = makeStyles(() => ({
   root: {
@@ -35,47 +35,6 @@ const useStyles = makeStyles(() => ({
     objectPosition: '-20rem',
     objectFit: 'cover',
     maxWidth: '50vw',
-  },
-  sitterLoginSignup: {
-    position: 'fixed',
-    display: 'flex',
-    flexDirection: 'row',
-    marginTop: '2rem',
-    marginRight: '4rem',
-    right: 0,
-    top: 0,
-  },
-  becomeASitterText: {
-    alignSelf: 'center',
-    fontSize: 20,
-    color: 'white',
-    marginRight: '4rem',
-  },
-  signupButton: {
-    marginLeft: '1rem',
-    width: '10rem',
-    height: '4rem',
-    backgroundColor: theme.palette.primary.main,
-    color: 'white',
-    fontSize: 18,
-    fontWeight: 600,
-  },
-  signupButtonText: {
-    color: 'white',
-    textDecoration: 'none',
-  },
-  loginButton: {
-    color: 'white',
-    border: '1px solid',
-    borderColor: 'white',
-    width: '10rem',
-    height: '4rem',
-    fontSize: 18,
-    fontWeight: 600,
-  },
-  loginButtonText: {
-    color: 'white',
-    textDecoration: 'none',
   },
 }));
 
