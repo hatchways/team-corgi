d739664dec83950d8f0e87e135a9a348dbb14262
hiteshparate <hiteshparate77@gmail.com>
2021-06-18 11:18:02 -0400

    removed fragments which were unnecessary

--- client/src/components/EditProfile/EditProfile.tsx
+++ client/src/components/EditProfile/EditProfile.tsx
@@ -20,89 +20,87 @@ const EditProfile = (props: Props): JSX.Element => {
   const classes = useStyles();
 
   return (
-    <>
-      <Grid container>
-        <Grid item xs={2}>
-          <Sidebar></Sidebar>
-        </Grid>
-        <Grid item xs={10} style={{ backgroundColor: '#FAFAFA' }}>
-          <Paper elevation={3} className={classes.gridContainer}>
-            <Typography variant="h3" className={classes.heading}>
-              Edit Profile
-            </Typography>
-            <form>
-              <Grid container>
-                <Grid item xs={2} md={2}></Grid>
-                <Grid item xs={2} md={2} className={classes.formLabel}>
-                  FIRST NAME
-                </Grid>
-                <Grid item xs={6} md={6} className={classes.formInputField}>
-                  <TextField size="small" variant="outlined" placeholder="First Name" fullWidth></TextField>
-                </Grid>
-                <Grid xs={2} md={2}></Grid>
-
-                <Grid item xs={2}></Grid>
-                <Grid item xs={2} className={classes.formLabel}>
-                  LAST NAME
-                </Grid>
-                <Grid item xs={6} className={classes.formInputField}>
-                  <TextField size="small" variant="outlined" placeholder="Last Name" fullWidth></TextField>
-                </Grid>
-                <Grid xs={2}></Grid>
-
-                <Grid item xs={2}></Grid>
-                <Grid item xs={2} className={classes.formLabel}>
-                  EMAIL ADDRESS
-                </Grid>
-                <Grid item xs={6} className={classes.formInputField}>
-                  <TextField size="small" variant="outlined" placeholder="johndoe@gmail.com" fullWidth></TextField>
-                </Grid>
-                <Grid xs={2}></Grid>
-
-                <Grid item xs={2}></Grid>
-                <Grid item xs={2} className={classes.formLabel}>
-                  PHONE NUMBER
-                </Grid>
-                <Grid item xs={6} className={classes.formInputField}>
-                  <Button color="primary" variant="outlined" className={classes.buttonPhone}>
-                    Add a phone number
-                  </Button>
-                </Grid>
-                <Grid xs={2}></Grid>
-
-                <Grid item xs={2}></Grid>
-                <Grid item xs={2} className={classes.formLabel}>
-                  WHERE YOU LIVE
-                </Grid>
-                <Grid item xs={6} className={classes.formInputField}>
-                  <TextField size="small" variant="outlined" placeholder="Toronto, Canada" fullWidth></TextField>
-                </Grid>
-                <Grid xs={2}></Grid>
-
-                <Grid item xs={1}></Grid>
-                <Grid item xs={3} className={classes.formLabel}>
-                  DESCRIBE YOURSELF
-                </Grid>
-                <Grid item xs={6} className={classes.formInputField}>
-                  <TextField
-                    size="small"
-                    variant="outlined"
-                    multiline
-                    rows={4}
-                    placeholder="About you"
-                    fullWidth
-                  ></TextField>
-                </Grid>
-                <Grid xs={2}></Grid>
-              </Grid>
-            </form>
-            <Button color="primary" size="large" type="submit" variant="contained" className={classes.submitButton}>
-              Submit
-            </Button>
-          </Paper>
-        </Grid>
+    <Grid container>
+      <Grid item xs={2}>
+        <Sidebar></Sidebar>
       </Grid>
-    </>
+      <Grid item xs={10} style={{ backgroundColor: '#FAFAFA' }}>
+        <Paper elevation={3} className={classes.gridContainer}>
+          <Typography variant="h3" className={classes.heading}>
+            Edit Profile
+          </Typography>
+          <form>
+            <Grid container>
+              <Grid item xs={2} md={2}></Grid>
+              <Grid item xs={2} md={2} className={classes.formLabel}>
+                FIRST NAME
+              </Grid>
+              <Grid item xs={6} md={6} className={classes.formInputField}>
+                <TextField size="small" variant="outlined" placeholder="First Name" fullWidth></TextField>
+              </Grid>
+              <Grid xs={2} md={2}></Grid>
+
+              <Grid item xs={2}></Grid>
+              <Grid item xs={2} className={classes.formLabel}>
+                LAST NAME
+              </Grid>
+              <Grid item xs={6} className={classes.formInputField}>
+                <TextField size="small" variant="outlined" placeholder="Last Name" fullWidth></TextField>
+              </Grid>
+              <Grid xs={2}></Grid>
+
+              <Grid item xs={2}></Grid>
+              <Grid item xs={2} className={classes.formLabel}>
+                EMAIL ADDRESS
+              </Grid>
+              <Grid item xs={6} className={classes.formInputField}>
+                <TextField size="small" variant="outlined" placeholder="johndoe@gmail.com" fullWidth></TextField>
+              </Grid>
+              <Grid xs={2}></Grid>
+
+              <Grid item xs={2}></Grid>
+              <Grid item xs={2} className={classes.formLabel}>
+                PHONE NUMBER
+              </Grid>
+              <Grid item xs={6} className={classes.formInputField}>
+                <Button color="primary" variant="outlined" className={classes.buttonPhone}>
+                  Add a phone number
+                </Button>
+              </Grid>
+              <Grid xs={2}></Grid>
+
+              <Grid item xs={2}></Grid>
+              <Grid item xs={2} className={classes.formLabel}>
+                WHERE YOU LIVE
+              </Grid>
+              <Grid item xs={6} className={classes.formInputField}>
+                <TextField size="small" variant="outlined" placeholder="Toronto, Canada" fullWidth></TextField>
+              </Grid>
+              <Grid xs={2}></Grid>
+
+              <Grid item xs={1}></Grid>
+              <Grid item xs={3} className={classes.formLabel}>
+                DESCRIBE YOURSELF
+              </Grid>
+              <Grid item xs={6} className={classes.formInputField}>
+                <TextField
+                  size="small"
+                  variant="outlined"
+                  multiline
+                  rows={4}
+                  placeholder="About you"
+                  fullWidth
+                ></TextField>
+              </Grid>
+              <Grid xs={2}></Grid>
+            </Grid>
+          </form>
+          <Button color="primary" size="large" type="submit" variant="contained" className={classes.submitButton}>
+            Submit
+          </Button>
+        </Paper>
+      </Grid>
+    </Grid>
   );
 };
 
--- client/src/components/Sidebar/SideBar.tsx
+++ client/src/components/Sidebar/SideBar.tsx
@@ -10,38 +10,36 @@ const Sidebar = (): JSX.Element => {
   const classes = useStyles();
 
   return (
-    <>
-      <Drawer
-        className={classes.drawer}
-        variant="permanent"
-        classes={{
-          paper: classes.drawerPaper,
-        }}
-        anchor="left"
-      >
-        <div className={classes.toolbar} />
-        <List>
-          <ListItem button component={Link} to="/editprofile">
-            <ListItemText primary="Edit Profile" className={classes.listItem}></ListItemText>
-          </ListItem>
-          <ListItem button component={Link} to="/profilephoto">
-            <ListItemText primary="Profile Photo" className={classes.listItem}></ListItemText>
-          </ListItem>
-          <ListItem button component={Link} to="/availability">
-            <ListItemText primary="Availability" className={classes.listItem}></ListItemText>
-          </ListItem>
-          <ListItem button component={Link} to="/payment">
-            <ListItemText primary="Payment" className={classes.listItem}></ListItemText>
-          </ListItem>
-          <ListItem button component={Link} to="/security">
-            <ListItemText primary="Security" className={classes.listItem}></ListItemText>
-          </ListItem>
-          <ListItem button component={Link} to="/settings">
-            <ListItemText primary="Settings" className={classes.listItem}></ListItemText>
-          </ListItem>
-        </List>
-      </Drawer>
-    </>
+    <Drawer
+      className={classes.drawer}
+      variant="permanent"
+      classes={{
+        paper: classes.drawerPaper,
+      }}
+      anchor="left"
+    >
+      <div className={classes.toolbar} />
+      <List>
+        <ListItem button component={Link} to="/editprofile">
+          <ListItemText primary="Edit Profile" className={classes.listItem}></ListItemText>
+        </ListItem>
+        <ListItem button component={Link} to="/profilephoto">
+          <ListItemText primary="Profile Photo" className={classes.listItem}></ListItemText>
+        </ListItem>
+        <ListItem button component={Link} to="/availability">
+          <ListItemText primary="Availability" className={classes.listItem}></ListItemText>
+        </ListItem>
+        <ListItem button component={Link} to="/payment">
+          <ListItemText primary="Payment" className={classes.listItem}></ListItemText>
+        </ListItem>
+        <ListItem button component={Link} to="/security">
+          <ListItemText primary="Security" className={classes.listItem}></ListItemText>
+        </ListItem>
+        <ListItem button component={Link} to="/settings">
+          <ListItemText primary="Settings" className={classes.listItem}></ListItemText>
+        </ListItem>
+      </List>
+    </Drawer>
   );
 };
 
