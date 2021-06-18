e2d06c2a689fa290e5eb2f05b8f76b2b9e5faf9e
Ariel Ragetli <68292487+tsumori91@users.noreply.github.com>
2021-06-18 10:17:57 -0700

    Add FE of Profile picture uploading.

--- client/src/components/ProfilePhoto/useStyles.ts
+++ client/src/components/ProfilePhoto/useStyles.ts
@@ -5,6 +5,38 @@ const useStyles = makeStyles(() => ({
     margin: '5% 15% 0% 15%',
     minHeight: '500px',
   },
+  main: {
+    height: '100%',
+    minHeight: '500px',
+  },
+  title: {
+    fontSize: '20px',
+    fontWeight: 'bolder',
+    padding: '40px',
+  },
+  displayPic: {
+    height: '100px',
+    width: '100px',
+  },
+  buttonUpload: {
+    padding: '14px',
+    width: '70%',
+  },
+  explanation: {
+    color: 'grey',
+    padding: '20px 0px 40px 0px',
+    fontSize: 12,
+    width: '50%',
+    textAlign: 'center',
+  },
+  buttonDelete: {
+    margin: '40px',
+  },
+  textDelete: {
+    color: 'grey',
+    fontSize: 12,
+    paddingLeft: '7px',
+  },
 }));
 
 export default useStyles;
