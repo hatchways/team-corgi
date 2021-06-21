6b8f651100744836acf86155b1fadf5c3ce3cc14
C. S. Ale <c.s.alexiuk@gmail.com>
2021-06-21 14:35:10 -0400

    finished the second page card

--- client/src/components/BookSitter/BookSitter.tsx
+++ client/src/components/BookSitter/BookSitter.tsx
@@ -1,4 +1,16 @@
-import { Typography, Grid, CircularProgress, Paper, Card, CardMedia, CardContent, Box } from '@material-ui/core';
+import {
+  Typography,
+  Grid,
+  CircularProgress,
+  Paper,
+  Card,
+  CardMedia,
+  CardContent,
+  Box,
+  TextField,
+  Button,
+} from '@material-ui/core';
+import { Rating } from '@material-ui/lab';
 import { useAuth } from '../../context/useAuthContext';
 import house from '../../Images/house.jpg';
 import useStyles from './useStyles';
@@ -9,14 +21,40 @@ const BookSitter = (): JSX.Element => {
 
   return (
     <Card className={classes.mainCard}>
-      <Paper elevation={3}>
-        <form>
-          <Grid container direction="column">
-            <Grid item className={classes.price}></Grid>
-            <Grid item className={classes.stars}></Grid>
-            <Grid item className={classes.dropIn}></Grid>
-            <Grid item className={classes.dropOff}></Grid>
-            <Grid item className={classes.button}></Grid>
+      <Paper elevation={3} className={classes.paper}>
+        <form className={classes.form}>
+          <Grid container direction="column" className={classes.grid}>
+            <Grid item className={classes.price}>
+              <Typography variant="h4">$14/hr</Typography>
+            </Grid>
+            <Grid item className={classes.stars}>
+              <Rating name="read-only" value={2} readOnly />
+            </Grid>
+            <Typography className={classes.dropInLabel}>DROP-IN</Typography>
+            <Grid item className={classes.dropIn}>
+              <TextField
+                id="datetime-local"
+                type="datetime-local"
+                className={classes.dropInText}
+                variant="outlined"
+                InputLabelProps={{
+                  shrink: true,
+                }}
+              />
+            </Grid>
+            <Typography className={classes.dropOffLabel}>DROP-OFF</Typography>
+            <Grid item className={classes.dropOff}>
+            <TextField
+                id="datetime-local"
+                type="datetime-local"
+                className={classes.dropInText}
+                variant="outlined"
+                InputLabelProps={{
+                  shrink: true,
+                }}
+              />
+            </Grid>
+            <Grid item className={classes.buttonGrid}><Button className={classes.button} >SEND REQUEST</Button></Grid>
           </Grid>
         </form>
       </Paper>
--- client/src/components/BookSitter/useStyles.ts
+++ client/src/components/BookSitter/useStyles.ts
@@ -5,22 +5,58 @@ import { relative } from 'path';
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
     mainCard: {
+      height: '400px',
+      width: '275px',
+      display: 'flex',
+    },
+    grid: {
+      flex: 1,
+    },
+    paper: {
+      flex: 1,
+      padding: '20px',
+    },
+    form: {
       flex: 1,
     },
     price: {
-      marginTop: '2rem',
+      textAlign: 'center',
     },
     stars: {
-      flex: 1,
+      marginTop: '5px',
+      display: 'flex',
+      justifyContent: 'center',
+      marginBottom: '40px',
+    },
+    dropInLabel: {
+      fontSize: 14,
+      fontWeight: 700,
     },
     dropIn: {
-      flex: 1,
+      display: 'flex',
+      marginBottom: '25px',
+    },
+    dropInText: {
+
+      borderColor: 'gray',
+    },
+    dropOffLabel: {
+      fontWeight: 700,
     },
     dropOff: {
-      flex: 1,
+      display: 'flex',
+    },
+    buttonGrid: {
+      display: 'flex',
+      justifyContent: 'center',
+      marginTop: '30px',
     },
     button: {
-      flex: 1,
+      color: 'white',
+      backgroundColor: theme.palette.primary.main,
+      borderRadius: '0px',
+      height: '45px',
+      width: '160px',
     },
   }),
 );
