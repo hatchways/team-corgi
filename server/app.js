59264171f52edb24e9e6d415c37a6ebcd36d545c
Ariel Ragetli <68292487+tsumori91@users.noreply.github.com>
2021-06-21 11:50:48 -0700

    Fix spelling mistakes

--- server/app.js
+++ server/app.js
@@ -44,7 +44,7 @@ app.use((req, res, next) => {
 
 app.use("/auth", authRouter);
 app.use("/users", userRouter);
-app.use("/profiles", profileRouter);
+app.use("/profile", profileRouter);
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "/client/build")));
