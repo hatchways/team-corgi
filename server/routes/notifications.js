5c9827d125793dce5541bf5c29036ef9381b3078
hiteshparate <hiteshparate77@gmail.com>
2021-06-22 16:18:11 -0400

    minor bug fix

--- server/routes/notifications.js
+++ server/routes/notifications.js
@@ -1,9 +1,9 @@
 const express = require('express');
-const { getAllnotifications, createNotification, readNotification, getUnreadNotifications } = require('../controllers/notifications');
+const { getAllNotifications, createNotification, readNotification, getUnreadNotifications } = require('../controllers/notifications');
 const router = express.Router();
 
 
-router.route('/all/:id').get(getAllnotifications);
+router.route('/all/:id').get(getAllNotifications);
 router.route('/').post(createNotification);
 router.route('/:id').get(readNotification);
 router.route('/unread/:id').get(getUnreadNotifications);
