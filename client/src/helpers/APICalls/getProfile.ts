0316f98c0319af3a3950898299b08e64885b41c6
hiteshparate <hiteshparate77@gmail.com>
2021-07-05 18:20:48 -0400

    interface change id(2 time) in User

--- client/src/helpers/APICalls/getProfile.ts
+++ client/src/helpers/APICalls/getProfile.ts
@@ -1,9 +1,7 @@
 import { ProfileApiData } from '../../interface/AuthApiData';
 import { FetchOptions } from '../../interface/FetchOptions';
 
-
 const getUserProfile = async (id: string): Promise<ProfileApiData> => {
-
   const fetchOptions: FetchOptions = {
     method: 'GET',
     headers: { 'Content-Type': 'application/json' },
@@ -11,7 +9,6 @@ const getUserProfile = async (id: string): Promise<ProfileApiData> => {
   };
 
   return await fetch(`/profile/userProfile/${id}`, fetchOptions)
-
     .then((res) => res.json())
     .catch(() => ({
       error: { message: 'Unable to connect to server. Please try again' },
@@ -19,4 +16,3 @@ const getUserProfile = async (id: string): Promise<ProfileApiData> => {
 };
 
 export default getUserProfile;
-
