6d0c9b55c97b5c36d06079a94e83e79c43c47f08
hiteshparate <hiteshparate77@gmail.com>
2021-07-05 23:45:39 -0400

    bug fixes for notifications and profile

--- client/src/components/DashboardCoponent/Header.jsx
+++ client/src/components/DashboardCoponent/Header.jsx
@@ -3,6 +3,12 @@ import { AppBar, Toolbar, Button } from '@material-ui/core';
 import React from 'react';
 import Logo from './logo.png';
 import { makeStyles } from '@material-ui/styles';
+import Popover from '@material-ui/core/Popover';
+import NotificationsIcon from '@material-ui/icons/Notifications';
+import PopupState, { bindTrigger, bindPopover } from 'material-ui-popup-state';
+import Tooltip from '@material-ui/core/Tooltip';
+import { Fab } from '@material-ui/core';
+import Notification from '../../components/Notification/Notification';
 
 const useStyles = makeStyles({
   box: {
@@ -39,6 +45,31 @@ function Header() {
         <Button variant="contained" color="secondary" className={classes.button}>
           Sign Up
         </Button>
+
+        <PopupState variant="popover" popupId="demo-popup-popover">
+          {(popupState) => (
+            <div>
+              <Tooltip title="notification">
+                <Fab {...bindTrigger(popupState)} color="primary">
+                  <NotificationsIcon></NotificationsIcon>
+                </Fab>
+              </Tooltip>
+              <Popover
+                {...bindPopover(popupState)}
+                anchorOrigin={{
+                  vertical: 'bottom',
+                  horizontal: 'center',
+                }}
+                transformOrigin={{
+                  vertical: 'top',
+                  horizontal: 'center',
+                }}
+              >
+                <Notification></Notification>
+              </Popover>
+            </div>
+          )}
+        </PopupState>
       </Toolbar>
     </AppBar>
   );
--- client/src/helpers/APICalls/getProfile.ts
+++ client/src/helpers/APICalls/getProfile.ts
@@ -1,9 +1,7 @@
 import { ProfileApiData } from '../../interface/AuthApiData';
 import { FetchOptions } from '../../interface/FetchOptions';
 
-
-const getUserProfile = async (id: string): Promise<ProfileApiData> => {
-
+const getProfile = async (id: string | undefined): Promise<ProfileApiData> => {
   const fetchOptions: FetchOptions = {
     method: 'GET',
     headers: { 'Content-Type': 'application/json' },
@@ -11,12 +9,10 @@ const getUserProfile = async (id: string): Promise<ProfileApiData> => {
   };
 
   return await fetch(`/profile/userProfile/${id}`, fetchOptions)
-
     .then((res) => res.json())
     .catch(() => ({
       error: { message: 'Unable to connect to server. Please try again' },
     }));
 };
 
-export default getUserProfile;
-
+export default getProfile;
